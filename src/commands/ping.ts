5cd4dd9646ac2ccab66e82f566ca6aaccdb24fd4
loveisglitchy <uncleneedham@gmail.com>
2022-11-25 14:49:02 +0000

    edit perm check plugin

--- src/commands/ping.ts
+++ src/commands/ping.ts
@@ -1,17 +1,10 @@
 import { commandModule, CommandType } from "@sern/handler";
 import { publish } from "../plugins/publish";
-import { permCheck } from "../plugins/myPermCheck";
+import { requirePermission } from "../plugins/myPermCheck";
 
 export default commandModule({
   type: CommandType.Both,
-  plugins: [
-    publish(),
-    permCheck(
-      "bot",
-      "ManageChannels",
-      `I cannot use this command. Please give me required permissions.`
-    ),
-  ],
+  plugins: [publish(), requirePermission("user", "ManageChannels")],
   description: "A ping command",
   execute: async (ctx, args) => {
     await ctx.reply("Pong 🏓");
